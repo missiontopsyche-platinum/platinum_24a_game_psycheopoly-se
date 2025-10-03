9fe3643598858f4078765c439cc2dfe4799443c5
hdathert <hdathert@asu.edu>
2025-10-03 11:35:32 -0700

    #4 #5 Adds Test Runner and Code Coverage
    Test Runner was added by default to the project, so
    I just had to add Code Coverage. I wrote a quick
    dry test to make sure the framework is set up
    correctly, and to test Code Coverage. We won't be
    tracking coverage reports on the repo for now.
    
    In my documentation, I'll go over how to ensure
    Code Coverage is on and working.

---
+++ Assets/Tests/EditMode/_TestFrameworkVerification.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Tests.EditMode
+{
+	/// <summary>
+	/// Sanity checks to ensure that the Unity Testing Framework is set up correctly.
+	/// These tests should always pass if all is correctly configured.
+	/// </summary>
+	public class _TestFrameworkVerification
+	{
+		[Test]
+		public void TestFramework_IsConfigured()
+		{
+			Assert.IsTrue(true, "Test framework should be operational.");
+		}
+
+		[Test]
+		public void TestFramework_CanPerformBasicMath()
+		{
+			int result = 2 + 2;
+			Assert.AreEqual(4, result, "Basic math operations should work in tests.");
+		}
+
+		[Test]
+		public void TestFramework_CanCreateUnityObjects()
+		{
+			GameObject testObject = new GameObject("TestObject");
+
+			Assert.IsNotNull(testObject, "Should be able to create GameObjects in tests.");
+			Assert.AreEqual("TestObject", testObject.name, "GameObject should have correct name.");
+
+			Object.DestroyImmediate(testObject);
+		}
+	}
+}
