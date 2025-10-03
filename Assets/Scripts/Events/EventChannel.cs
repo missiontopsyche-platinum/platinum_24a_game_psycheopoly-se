64b463786e7856703dd1524d9cc97f2b16a3be7c
hdathert <hdathert@asu.edu>
2025-10-03 13:30:35 -0700

    #18-21 Implements EventChannel<T> This class should be used as the basis for all event channels. We'll be using concrete 'implementations' to allow us to create these in the Asset Menu by declaring versions of this class with specific types, instead of generic `T`.
    For instance:
    `public class IntEventChannel : EventChannel<int> {}`
    would allow us to use
    `[CreateAssetMenu(menuName = "Events/Int Event Channel")]`
    as a tag to allow us to create new
    channels with that data type as the
    argument to the invoking methods.
    
    We will define different kinds of
    data type channels in a separate
    file.

--- Assets/Scripts/Events/EventChannel.cs
+++ Assets/Scripts/Events/EventChannel.cs
@@ -1,12 +1,41 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class EventChannel<T> : ScriptableObject
 {
-    private event Action<T> OnEventRaised;
+    private readonly List<Action<T>> listeners = new List<Action<T>>();
 
-    public void Subscribe(Action<T> listener) {}
-    public void Unsubscribe(Action<T> listener) {}
-    public void RaiseEvent(T data) {}
-    public void ClearAllListeners() {}
+    public void Subscribe(Action<T> listener)
+    {
+        if (listener == null)
+        {
+            Debug.LogWarning("Attempted to subscribe null listener on " + this.name);
+            return;
+        }
+        if (!listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    public void Unsubscribe(Action<T> listener)
+    {
+        if (listener == null)
+            return;
+        listeners.Remove(listener);
+    }
+
+    public void RaiseEvent(T data)
+    {
+        foreach (Action<T> listener in listeners)
+        {
+            listener?.Invoke(data);
+        }
+    }
+
+    public void ClearAllListeners()
+    {
+        listeners.Clear();
+    }
 }
