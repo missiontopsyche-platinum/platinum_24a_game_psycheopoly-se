8840e09aea9e9085812312831b5bb48822e30669
hdathert <hdathert@asu.edu>
2025-10-20 10:08:20 -0700

    US203 T205 Hooks BoardRenderer to BoardManager to get Space data

--- Assets/Scripts/Board/SpaceRenderer.cs
+++ Assets/Scripts/Board/SpaceRenderer.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+using Space = PsycheOpoly.Board.Space;
 using Random = UnityEngine.Random;
 
 public class SpaceRenderer : MonoBehaviour
 {
+    [SerializeField] private Space space;
     [SerializeField] public Color color;
-    [SerializeField] private MeshRenderer renderer;
+    [SerializeField] private MeshRenderer meshRenderer;
 
     private void Awake()
     {
-        // until we have actual Space paramenters as ScriptableObjects, we'll have random colors.
+        // until we have actual Space parameters as ScriptableObjects, we'll have random colors.
         color = Random.ColorHSV(0,1,0,1,0,1,1,1);
     }
 
-    public void SetUpSpace(float scale)
+    public void SetUpSpace(Space space, float scale)
     {
+        this.space = space;
         // scale the space relative to board/camera size
         transform.localScale *= scale;
-        renderer.material.color = color;
+        meshRenderer.material.color = color;
+
+        name = space.Name;
     }
 }
