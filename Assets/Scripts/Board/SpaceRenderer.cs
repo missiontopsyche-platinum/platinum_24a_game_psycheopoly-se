d9ca9789e61854c1d6a0bd4cb61e2af9188ca05c
hdathert <hdathert@asu.edu>
2025-10-22 16:25:08 -0700

    US203 T206 Adds input manager to track mouse clicks/hovers on spaces

--- Assets/Scripts/Board/SpaceRenderer.cs
+++ Assets/Scripts/Board/SpaceRenderer.cs
@@ -1,27 +1,89 @@
+using Interactable;
+using Logging;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Space = PsycheOpoly.Board.Space;
 using Random = UnityEngine.Random;
 
-public class SpaceRenderer : MonoBehaviour
+public class SpaceRenderer : InteractableGameObject
 {
-    [SerializeField] private Space space;
     [SerializeField] public Color color;
     [SerializeField] private MeshRenderer meshRenderer;
 
+    private Space space;
+    
+    // debug timer - remove when we add actual functionality.
+    private float hoverTime = 1f;
+    private float hoverTimer = 0f;
+    
+    // to add: EventChannels to communicate with our other elements
+    
     private void Awake()
     {
         // until we have actual Space parameters as ScriptableObjects, we'll have random colors.
         color = Random.ColorHSV(0,1,0,1,0,1,1,1);
     }
 
-    public void SetUpSpace(Space space, float scale)
+    public void SetUpSpace(Space spaceData, float scale)
     {
-        this.space = space;
+        space = spaceData;
         // scale the space relative to board/camera size
         transform.localScale *= scale;
         meshRenderer.material.color = color;
 
         name = space.Name;
+
+        // ensure box collider
+        BoxCollider boxCollider = gameObject.GetComponent<BoxCollider>();
+        if (!boxCollider)
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.size = new Vector3(scale, scale, 1f);
+    }
+
+    public override void OnLeftClick()
+    {
+        Logging.Logger.Info(
+            $"SpaceRenderer: {name}", 
+            "On Left Click", 
+            LogCategory.UI, this);
+    }
+    
+    public override void OnRightClick()
+    {
+        Logging.Logger.Info(
+            $"SpaceRenderer: {name}", 
+            "On Right Click", 
+            LogCategory.UI, this);
+    }
+    
+    public override void OnHoverEnter()
+    {
+        Logging.Logger.Info(
+            $"SpaceRenderer: {name}", 
+            "On Hover Enter", 
+            LogCategory.UI, this);
+    }
+    
+    public override void OnHoverExit()
+    {
+        Logging.Logger.Info(
+            $"SpaceRenderer: {name}", 
+            "On Hover Exit", 
+            LogCategory.UI, this);
+        
+        hoverTimer = 0f; // reset hover timer, remove later
+    }
+
+    public override void OnHover()
+    {
+        // this is all temporary to throw logs to indicate hovering is being checked.
+        hoverTimer -= Time.deltaTime;
+        if (hoverTimer < 0)
+        {
+            Logging.Logger.Info(
+                $"SpaceRenderer: {name}", 
+                "On Hover (current)", 
+                LogCategory.UI, this);
+            hoverTimer = hoverTime;
+        }
     }
 }
