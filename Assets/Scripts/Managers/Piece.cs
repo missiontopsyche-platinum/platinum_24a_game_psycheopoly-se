b1ab91fe822159a90366b953b1f8c21829d9264c
nicholasnastase <nastasenick22@gmail.com>
2025-10-23 17:01:19 -0400

    Created a pretty simple Piece monobehavior that should be able to get the actual piece to move. Going back to read a bit up on GameManager and PlayerManager to see if anything needs to change in Piece.cs before submitting any changes.

--- Assets/Scripts/Managers/Piece.cs
+++ Assets/Scripts/Managers/Piece.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
+using System.Collections;
 
-public class Piece
+public class Piece : MonoBehaviour
 {
-    
+    private float moveSpeed = 3f;
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveRoutine(targetPosition));
+    }
+
+    private IEnumerator MoveRoutine(Vector3 target)
+    {
+        while (Vector3.Distance(transform.position, target) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position, target, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = target;
+    }
+
 }
