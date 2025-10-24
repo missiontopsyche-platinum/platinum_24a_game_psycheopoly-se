f00654bfea7734de16948736dbd8d47250b4772b
nicholasnastase <nastasenick22@gmail.com>
2025-10-24 13:05:57 -0400

    Added test function, also adjusted how the Piece.cs moves the piece in general to make sure i'm adjusting for time correctly

--- Assets/Scripts/Managers/Piece.cs
+++ Assets/Scripts/Managers/Piece.cs
@@ -13,14 +13,22 @@ public class Piece : MonoBehaviour
 
     private IEnumerator MoveRoutine(Vector3 target)
     {
+        //restructure how time elapses to test for runnabilty
+        //assume 50 fps if Time.deltaTime == 0
+        float simulatedDelta = Time.deltaTime > 0 ? Time.deltaTime : 0.02f; 
+
         while (Vector3.Distance(transform.position, target) > 0.01f)
         {
             transform.position = Vector3.MoveTowards(
-                transform.position, target, moveSpeed * Time.deltaTime);
+                transform.position,
+                target,
+                moveSpeed * simulatedDelta
+            );
             yield return null;
         }
 
         transform.position = target;
     }
+
 
 }
