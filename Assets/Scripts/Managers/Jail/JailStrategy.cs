6995c9ae4d0befae785c9003c1cbf32a0d5d97da
nicholasnastase <nastasenick22@gmail.com>
2025-11-05 19:00:38 -0500

    Added methods to interface to be accessed in the StandardJailStrategy for t286

--- Assets/Scripts/Managers/Jail/JailStrategy.cs
+++ Assets/Scripts/Managers/Jail/JailStrategy.cs
@@ -8,6 +8,16 @@ namespace Assets.Scripts.Managers.Jail
 {
     public interface JailStrategy
     {
-        
+        //this is for the rollng doubles option, players do this every turn
+        void AttemptEscape(Player player, int dice1, int dice2);
+
+        //this will be done to escape jail early or can also be used when forced to exit on 3rd chance
+        void PayFee(Player player);
+
+        //this is the get out of ajial free card
+        void UseGetOutOfJailFree(Player player);
+
+        //and this will only occur once the player is on their third round in jail
+        void ForcedExit(Player player);
     }
 }
