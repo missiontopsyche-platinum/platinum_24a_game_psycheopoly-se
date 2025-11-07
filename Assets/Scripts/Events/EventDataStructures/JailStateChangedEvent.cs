3ff131b2bd8760eca828156f8d295c6a8e430569
nicholasnastase <nastasenick22@gmail.com>
2025-11-07 13:44:06 -0500

    Added the JailStateChangedEvent to be a data container for the t288

---
+++ Assets/Scripts/Events/EventDataStructures/JailStateChangedEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// this event channel just a data container for the event of chaning jail status changed 
+/// </summary>
+namespace Assets.Scripts.Events.EventDataStructures
+{
+    [System.Serializable]
+    public class JailStateChangedEvent
+    {
+        public Player player;
+        public bool inJail;
+        public int jailTurns;
+
+        public JailStateChangedEvent(Player player, bool inJail, int jailTurns)
+        {
+            this.player = player;
+            this.inJail = inJail;
+            this.jailTurns = jailTurns;
+        }
+    }
+}
