178c6f03ff45646868d162b033ce5237c3ad4721
hdathert <hdathert@asu.edu>
2025-11-08 15:38:08 -0800

    US322 T330 Implements jail spaces (go for launch/launch pad)

---
+++ Assets/Scripts/Data/SpaceData/GoForLaunchSpaceData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Events.EventDataStructures;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoForLaunchSpaceData", menuName = "Board Spaces/Go For Launch Space")]
+public class GoForLaunchSpaceData : SpaceData
+{
+    [SerializeField] public String flavorText = "GO FOR LAUNCH!";
+    [SerializeField] public PlayerEventChannel goDirectlyToLaunchPadChannel;
+    
+    public override void OnLanded(Player player)
+    {
+        // call event to send player to launch pad (jail)
+        goDirectlyToLaunchPadChannel.RaiseEvent(player);
+    }
+
+    public override void OnPassed(Player player)
+    {
+        // do nothing
+    }
+
+    public override SpaceHoverEvent OnHover()
+    {
+        SpaceHoverEvent payload = base.OnHover();
+
+        payload.AppendInformation(flavorText);
+        payload.AppendInformation("Go directly to the Launch Pad, do not pass GO.");
+        
+        return payload;
+    }
+}
