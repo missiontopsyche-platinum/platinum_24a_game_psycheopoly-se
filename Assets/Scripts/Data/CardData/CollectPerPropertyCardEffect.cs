cd00b3c472f36e2008a23bb70a2afa627eee6106
kescob0825 <escobar.karl0825@gmail.com>
2025-11-11 14:41:10 -0700

    Refactored Pay/Collect per property class to allow two fields: Charge for houses, and charge for hotels

--- Assets/Scripts/Data/CardData/CollectPerPropertyCardEffect.cs
+++ Assets/Scripts/Data/CardData/CollectPerPropertyCardEffect.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 
 public class CollectPerPropertyCardEffect : CardEffect
 {
-    [SerializeField] public int Amount = 0;
+    [SerializeField] public int ChargeForHouse = 0;
+    [SerializeField] public int ChargeForHotel = 0;
     //[SerializeField] public ChargePlayerEventChannel chargePlayerEventChannel; TODO: This must be refactored to target bank instead of player
     [SerializeField] public PayPlayerEventChannel payPlayerEventChannel;
     public override void ApplyEffect(Player player)
     {
-        if (!isValidPlayer(player)) return;
-        int propertyCount = player.GetOwnedProperties().Count;
-        if (propertyCount <= 0 || Amount <= 0) return;
-        int totalAmount = propertyCount * Amount;
+        if (!isValidPlayer(player) 
+            || player.GetOwnedProperties().Count <= 0 
+            || (ChargeForHouse <= 0 && ChargeForHotel <= 0)) return;
+
+        int houseCount = 0;
+        int hotelCount = 0;
+
+        foreach (var property in player.GetOwnedProperties())
+        {
+            if (property is PropertySpaceData)
+            {
+                PropertySpaceData propertySpace = (PropertySpaceData)property;
+                if (propertySpace.GetCurrentUpgradeLevel() == 0) continue;
+                if (propertySpace.GetCurrentUpgradeLevel() < 5) houseCount++;
+                else hotelCount++;
+            }
+        }
+        int totalAmount = (ChargeForHouse * houseCount) + (ChargeForHotel * hotelCount);
         // chargePlayerEventChannel.RaiseEvent(new ChargePlayerEvent(banker, totalAmount)); TODO: This should charge the bank or banker player
         payPlayerEventChannel.RaiseEvent(new PayPlayerEvent(player, totalAmount));
     }
--- Assets/Scripts/Data/CardData/PayPerPropertyCardEffect.cs
+++ Assets/Scripts/Data/CardData/PayPerPropertyCardEffect.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 
 public class PayPerPropertyCardEffect : CardEffect
 {
-    [SerializeField] public int Amount = 0;
+    [SerializeField] public int ChargeForHouse = 0;
+    [SerializeField] public int ChargeForHotel = 0;
     [SerializeField] public ChargePlayerEventChannel chargePlayerEventChannel;
     // [SerializeField] public PayPlayerEventChannel payPlayerEventChannel; TODO: This must be refactored to target bank instead of player
     public override void ApplyEffect(Player player)
     {
-        if (!isValidPlayer(player)) return;
-        int propertyCount = player.GetOwnedProperties().Count;
-        if (propertyCount <= 0 || Amount <= 0) return;
-        int totalAmount = propertyCount * Amount;
+        if (!isValidPlayer(player) 
+            || player.GetOwnedProperties().Count <= 0 
+            || (ChargeForHouse <= 0 && ChargeForHotel <= 0)) return;
+
+        int houseCount = 0;
+        int hotelCount = 0;
+
+        foreach (var property in player.GetOwnedProperties())
+        {
+            if (property is PropertySpaceData)
+            {
+                PropertySpaceData propertySpace = (PropertySpaceData)property;
+                if (propertySpace.GetCurrentUpgradeLevel() == 0) continue;
+                if (propertySpace.GetCurrentUpgradeLevel() < 5) houseCount++;
+                else hotelCount++;
+            }
+        }
+        int totalAmount = (ChargeForHouse * houseCount) + (ChargeForHotel * hotelCount);
         chargePlayerEventChannel.RaiseEvent(new ChargePlayerEvent(player, totalAmount));
         // payPlayerEventChannel.RaiseEvent(new PayPlayerEvent(player, totalAmount)); TODO: This should pay the bank or banker player
     }
