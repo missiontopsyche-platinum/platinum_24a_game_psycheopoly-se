77405b6c8628f269dd62dff6f886e88175030a9b
dzadroga <dzadroga@asu.edu>
2025-11-15 13:24:42 -0500

    Completes task 384 which wires the ok button to execute the card and Hide the UI

--- Assets/Scripts/UI/Cards/CardPopupUI.cs
+++ Assets/Scripts/UI/Cards/CardPopupUI.cs
@@ -7,7 +7,7 @@ public class CardPopupUI : MonoBehaviour
     [SerializeField] private Text cardTitleText;
     [SerializeField] private Text cardBodyText;
     [SerializeField] private Button okButton;
-    [SerializeField] private CanvasGroup CanvasGroup;
+    [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private Image artworkImage;
     [SerializeField] private CardDrawnEventChannel cardDrawnChannel;
     private Card currentCard;
@@ -31,6 +31,13 @@ public class CardPopupUI : MonoBehaviour
         }
     }
 
+    //Connects the OK button to code
+    private void Awake()
+    {
+        okButton.onClick.AddListener(OnOkClicked); 
+    }
+
+    //called when CardDeck raises the CardDrawn Event
     private void OnCardDrawn(Card card, Player player, CardDeck deck)
     {
         currentCard = card;
@@ -54,17 +61,17 @@ public class CardPopupUI : MonoBehaviour
             //Assign the UI feilds from card Scriptable Object
             if(cardTitleText != null) 
             {
-                cardTitleText.text = card.title;
+                cardTitleText.text = card.title; //use real names
             }
 
             if(cardBodyText != null)
             {
-                cardBodyText.text = card.bodyText;
+                cardBodyText.text = card.bodyText; //use real description
             }
 
             if(artworkImage != null)
             {
-                artworkImage.sprite = card.artwork;
+                artworkImage.sprite = card.artwork; //if we want to add images
             }
 
             //Fade in
@@ -76,4 +83,45 @@ public class CardPopupUI : MonoBehaviour
             //fadeRoutine = StartCoroutine(FaideIn());
         }
     }
+
+    private void OnOkClicked()
+    {
+        //If something went wrong just hide the card 
+        if(currentCard == null || currentPlayer == null || currentDeck == null)
+        {
+            HideInstant();
+            return;
+        }
+
+        //execute card effect 
+        foreach (var effect in currentCard.effect)
+        {
+            effect.ApplyEffect(currentPlayer);
+        }
+
+        //return to deck 
+        currentDeck.ReturnCardToDeck(currentCard);
+
+        //Clear the states
+        currentCard = null;
+        currentPlayer = null;
+        currentDeck = null;
+
+        //Fade out
+        //StartHideAnimation();
+    }
+
+    //Helper to instantly hide without any animation 
+    private void HideInstant()
+    {
+        if(canvasGroup != null)
+        {
+            canvasGroup.alpha = 0f;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        gameObject.SetActive(false);
+    }
+
 }
