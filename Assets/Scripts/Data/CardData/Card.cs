ba1f056420da7dc6a35b24a4251c46ef3bd29900
dzadroga <dzadroga@asu.edu>
2025-11-15 13:07:28 -0500

    Completes task 383 which implements the ShowCard() method to display the card data.

--- Assets/Scripts/Data/CardData/Card.cs
+++ Assets/Scripts/Data/CardData/Card.cs
@@ -7,6 +7,10 @@ public class Card : ScriptableObject
 {
     public string title;
     public string bodyText;
+    //This may be changed later on if we decide not to use images
+    //On all of the cards but for now I added it since it is easier
+    //To delete later than try to add it
+    public Sprite artwork; 
 
     [SerializeReference]
     public List<CardEffect> effect;
