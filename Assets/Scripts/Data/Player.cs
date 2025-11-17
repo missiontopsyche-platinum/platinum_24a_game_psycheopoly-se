a85bf78c3433c679f412a789c0eabc6a996f866b
kescob0825 <escobar.karl0825@gmail.com>
2025-11-17 15:01:23 -0700

    US360 Merge Conflict with dev resolution

--- Assets/Scripts/Data/Player.cs
+++ Assets/Scripts/Data/Player.cs
@@ -21,8 +21,9 @@ public class Player : ScriptableObject
     [SerializeField] private int getOutOfJailFree_Chance;
     [SerializeField] private int getOutOfJailFree_Community;
     private List<OwnableSpaceData> ownedProperties = new();
+    private List<Card> getOutOfJailCards = new();
 
-    
+
     private Color color;
 
     public void SetId(int id)
