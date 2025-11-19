24bca279f8688a938b827b52e6e352cdeb9f6454
nicholasnastase <nastasenick22@gmail.com>
2025-11-18 20:31:30 -0500

    add play-testing for US371

---
+++ Assets/Tests/PlayMode/CardEffects/CardEffectTestBase.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Assets.Scripts.Events.EventChannelTypes;
+
+namespace Assets.Tests.PlayMode.CardEffects
+{
+    /// <summary>
+    /// Shared helpers for card effect tests.
+    /// </summary>
+    public abstract class CardEffectTestBase
+    {
+        protected Player CreatePlayer(int id = 0, int money = 1500)
+        {
+            var player = ScriptableObject.CreateInstance<Player>();
+            player.SetId(id);
+            player.SetPName($"Player {id}");
+            player.SetMoney(money);
+            player.SetPosition(0);
+            return player;
+        }
+
+        protected TEffect CreateEffect<TEffect>() where TEffect : CardEffect
+        {
+            return ScriptableObject.CreateInstance<TEffect>();
+        }
+
+        protected TChannel CreateChannel<TChannel>() where TChannel : ScriptableObject
+        {
+            return ScriptableObject.CreateInstance<TChannel>();
+        }
+    }
+}
