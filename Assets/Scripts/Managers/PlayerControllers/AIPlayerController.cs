b5ec998e72547aa7927601d58c7fd1c1456fe41d
hdathert <hdathert@asu.edu>
2026-01-27 14:25:43 -0800

    US490 T491/492 Adds AIPlayerController and hooks up PurchaseRequest event

---
+++ Assets/Scripts/Managers/PlayerControllers/AIPlayerController.cs
@@ -0,0 +1,43 @@
+﻿using Events.EventDataStructures;
+
+namespace Managers.PlayerControllers
+{
+    public class AIPlayerController: PlayerController
+    {
+        // attributes
+        
+        // event channels ... I don't think this will need special ones
+
+        /// <summary>
+        /// Creates an AI player controller. This needs to be called in conjunction with <c>.Subscribe()</c>
+        /// so that event channels are properly routed.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="turnStarted"></param>
+        /// <param name="purchaseRequest"></param>
+        /// <param name="chargeOwnershipFee"></param>
+        /// <param name="passedGoPayment"></param>
+        public AIPlayerController(
+            Player player,
+            TurnStartedEventChannel turnStarted,
+            PurchaseOwnableRequestEventChannel purchaseRequest,
+            ChargeOwnershipFeeEventChannel chargeOwnershipFee,
+            PayPlayerEventChannel passedGoPayment) 
+            : base(player, turnStarted, purchaseRequest, chargeOwnershipFee, passedGoPayment)
+        {
+            // do AI specific set up here
+        }
+
+        public override void Subscribe()
+        {
+            base.Subscribe();
+            
+            purchaseOwnableRequestEventChannel.Subscribe(PurchaseRequestDecision);
+        }
+
+        private void PurchaseRequestDecision(PurchaseOwnableRequestEvent pore)
+        {
+            // todo implement
+        }
+    }
+}
