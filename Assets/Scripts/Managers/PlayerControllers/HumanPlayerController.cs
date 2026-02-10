795b04414efa3da90560d80be8ebcc67c4afa9b6
aldridge764@gmail.com <aldridge764@gmail.com>
2026-02-09 22:22:05 -0700

    Task 600 - add initial checks to trigger bankruptcy

--- Assets/Scripts/Managers/PlayerControllers/HumanPlayerController.cs
+++ Assets/Scripts/Managers/PlayerControllers/HumanPlayerController.cs
@@ -105,7 +105,6 @@ namespace Managers.PlayerControllers
                 if (controlledPlayer.IsBankrupt(cofe.amount))
                 {
                     // TODO: Call event channel for UI
-
                     controlledPlayer.ClearOwnership();
                     // Need to check with Hank to verify GameManager linkage. But currently no link, therefore we will create a "BankruptPlayer" event channel to fire.
                     // Will return an int, only providing the player ID which SHOULD be the turn order number.
