8e36c009a0c05099e16708c376a0271e29df821d
hdathert <hdathert@asu.edu>
2026-02-13 13:54:06 -0800

    US581 T668 Sets up mock object builders for testing AI Behaviors

---
+++ Assets/Tests/EditMode/PlayerControllerTests/PlayerControllerTestBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Tests.EditMode.PlayerControllerTests.Builders;
+using UnityEngine;
+
+namespace Tests.EditMode.PlayerControllerTests
+{
+    public abstract class PlayerControllerTestBase
+    {
+        private readonly List<ScriptableObject> createdInstances = new ();
+
+        protected MockPlayerBuilder APlayer() => new MockPlayerBuilder();
+        protected MockOwnableBuilder AnOwnableSpace() => new MockOwnableBuilder();
+
+        protected T Track<T>(T instance) where T : ScriptableObject
+        {
+            createdInstances.Add(instance);
+            return instance;
+        }
+
+        [TearDown]
+        protected virtual void TearDown()
+        {
+            foreach (var instance in createdInstances)
+                Object.DestroyImmediate(instance);
+            createdInstances.Clear();
+        }
+    }
+}
