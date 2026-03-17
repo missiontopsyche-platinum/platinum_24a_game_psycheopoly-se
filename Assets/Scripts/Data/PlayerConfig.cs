40ac93a0c8d7c95e1c2fcc38d9114648fabb5c45
hdathert <hdathert@asu.edu>
2026-03-17 13:33:43 -0700

    US841 T844 Adds PlayerConfig data class that will be used by PlayerManager to bootstrap PlayerController subclasses

---
+++ Assets/Scripts/Data/PlayerConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Data
+{
+    /// <summary>
+    /// This struct contains a package used by PlayerManager to build out PlayerControllers
+    /// with the correct sub-class (Human or AI), and is created in GameManager on game initialization
+    /// based on the Game Configuration that is set before the game starts.
+    /// </summary>
+    public class PlayerConfig
+    {
+        public readonly Player playerData;
+        public readonly bool isHuman;
+
+        public PlayerConfig(Player playerData, bool isHuman)
+        {
+            this.playerData = playerData;
+            this.isHuman = isHuman;
+        }
+    }
+}
