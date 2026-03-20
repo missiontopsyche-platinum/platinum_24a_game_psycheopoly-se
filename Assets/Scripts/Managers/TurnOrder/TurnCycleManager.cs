18e6c217e8f81f4bd307a6f7c692072e74a96c6d
hdathert <hdathert@asu.edu>
2026-03-20 12:08:48 -0700

    us819 T821 Moves TurnOrderStrategy methods into TurnCycleManager

--- Assets/Scripts/Managers/TurnOrder/TurnCycleManager.cs
+++ Assets/Scripts/Managers/TurnOrder/TurnCycleManager.cs
@@ -9,8 +9,7 @@ namespace Assets.Scripts.Managers.TurnOrder
         [Header("Config")]
         [SerializeField] private int playerCount = 4;
         [SerializeField] private int startingPlayerIndex = 0;
-
-        private StandardTurnOrderStrategy strategy = new ();
+        
         private PlayerTurnState playerTurnState;
 
         public int CurrentPlayerIndex { get; private set; }
@@ -32,7 +31,7 @@ namespace Assets.Scripts.Managers.TurnOrder
         //Moves to next player or repeats if extra turn 
         public int Advance()
         {
-            int next = strategy.NextPlayerIndex(CurrentPlayerIndex, playerCount, playerTurnState);
+            int next = NextPlayerIndex(CurrentPlayerIndex);
 
             CurrentPlayerIndex = next;
 
@@ -48,6 +47,44 @@ namespace Assets.Scripts.Managers.TurnOrder
         public void SyncCurrentPlayerIndex(int playerIndex)
         {
             CurrentPlayerIndex = Mathf.Clamp(playerIndex, 0, playerCount - 1);
+        }
+        
+        private bool HasExtraTurn(int currentIndex, PlayerTurnState state)
+        {
+            return state.GetExtraTurn(currentIndex);
+        }
+
+        private int NextPlayerIndex(int currentIndex)
+        {
+            //If the current player has an extra turn then clear it and keep same index
+            if (playerTurnState.GetExtraTurn(currentIndex))
+            {
+                playerTurnState.SetExtraTurn(currentIndex, false);
+                return currentIndex;
+            }
+
+            //Otherwise move forward until there is a playable player
+            int attempts = 0;
+            int idx = currentIndex;
+
+            while (attempts < playerCount)
+            {
+                idx = (idx + 1) % playerCount;
+                attempts++;
+
+                if (playerTurnState.IsEliminated(idx)) continue;
+
+                if (playerTurnState.IsSkippedThisRound(idx))
+                {
+                    playerTurnState.ConsumeSkip(idx);
+                    continue;
+                }
+
+                return idx; //found next
+            }
+
+            //If everyone is eliminated or skipped then just return current
+            return currentIndex;
         }
     }
 }
